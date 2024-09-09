749c39644da8177a7e0c6fd6faf733c36cb3a5ca
carlinho00 <00001105144562sp@al.educacao.sp.gov.br>
2024-09-09 19:21:24 -0300

    nova versão

---
+++ script.js
@@ -0,0 +1,12 @@
+// const avance=querySelectorA11(btn-proximo)
+avanca.fortach(buton)=>}
+GamepadButton.adventlistener(click;function()(
+    const atual= document.querySelector('.ativo);
+        const proximoPasso='passo'+this.getatribute('data-proximo');
+        atual.classlist.remove('ativo');
+        document.getlementBild(proximoPasso).classlist.add('ativo');
+        
+})
+})
+
+
